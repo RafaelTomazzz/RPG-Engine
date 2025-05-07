314b6be08e8f51eb6c5c81c4fdf3ecfe71617d88
RAFAEL TOMAZ DE OLIVEIRA AQUINO <CG3033791@car.ifsp>
2025-05-06 21:44:38 -0300

    ConecteiAoServer

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import personagemRouter from './Router/personagemRouter.js' 
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: "Funcionou"
+    })
+})
+
+//app.get('/personagem', personagemRouter)
+
+app.listen(3000, () => {
+    console.log('Servirdor Rodando no http://localhost:3000')
+})
